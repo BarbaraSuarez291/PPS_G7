fe447bda46f4a179f1ab24881c6c9c45f43a54aa
unknown <alex.larramendi14@gmail.com>
2022-07-14 18:44:28 -0300

    algo

--- bloquea_usu.php
+++ bloquea_usu.php
@@ -1,53 +1,21 @@
 <?php 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 include('./db/conexionDB.php');
+include('./funcion_delete.php');
 
-require './phpmailer/Exception.php';
-require './phpmailer/PHPMailer.php';
-require './phpmailer/SMTP.php';
-
-$mail = new PHPMailer(true);
-
-$correo = $_GET['email'];
+$email = $_GET['email'];
 
 $estado = "Bloqueado";
 
 $bloquear = "UPDATE reseñas SET estado='$estado' WHERE email='$correo'";
 $bloqueado = mysqli_query($conexion,$bloquear);
 
-try{
-    //Server settings
-    #$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'alex.larramendi14@gmail.com';                     //SMTP username
-    $mail->Password   = 'hlvlhjbjdoibecsw';                               //SMTP password
-    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-    $mail->SMTPOptions = array(
-        'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-        ));
-    //Recipients
-    $mail->setFrom('alex.larramendi14@gmail.com', 'Ballet De Jesus');
-    $mail->addAddress($correo);     //Add a recipient
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Recuperacion de contraseña';
-    $mail->Body    = "Usted ha sido bloqueado del sistema";
-
-    $mail->send();
-    echo 'El correo se envio correctamente';
-    header("Location: abm_usuarios.php");
-    
-}catch (Exception $e) {
-    echo 'Ocurrio el siguiente error: ', $mail->ErrorInfo;
-}
+$traer = "SELECT reseña  FROM reseñas WHERE email=$email";
+$reseña = mysqli_query($conexion,$traer); 
+while($fila_reseña = mysqli_fetch_array($reseña)){
+$mensaje = "Usted ha sido bloquedo del sistema de ballet de Jesus por el siguiente comentario:".
+           "`". $reseña . " ´";
+$asunto = "Aviso de bloqueo";
+envia_mail($mail,$email,$mensaje, $asunto);
+};
 mysqli_close($conexion);
 ?>
